f849b7e9a32037d103a17a27ebb659aa398d925d
Bengt Marten Agren <maraingist@me.com>
2019-09-03 21:35:57 +0800

    More rationals.

--- src/Rational.hs
+++ src/Rational.hs
@@ -5,11 +5,12 @@ import Data.List.Split (chunksOf)
 
 main :: IO ()
 main = do
-    let xs = nub $ sort [ x / y | x <- [1..9], y <- [1..9] ]
+    let xs = nub $ sort [ x / y | x <- [1..n], y <- [1..n] ]
+        n = 20
     mapM_ putStrLn $ output "rational" xs
 
 output :: String -> [Double] -> [String]
-output name xs = decl : (map ("    "++) $ (zipWith g [0..] $ map f $ chunksOf 32 xs) ++ [ "};", "" ])
+output name xs = decl : (map ("    "++) $ (zipWith g [0..] $ map f $ chunksOf 4 xs) ++ [ "};", "" ])
     where f = intercalate "," . map show
           g 0 s = "{ " ++ s
           g _ s = ", " ++ s
