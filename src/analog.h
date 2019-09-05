5c0869373f13b4b76aa887750c1b2e8333c59a61
Bengt Marten Agren <maraingist@me.com>
2019-09-05 22:24:50 +0800

    First reasonable cv tracking.

--- src/analog.h
+++ src/analog.h
@@ -18,34 +18,31 @@ typedef hal::timer::timer_t<4> adc_tim;
 typedef adc_t<1> adc;
 typedef dma_t<1> adc_dma;
 static const uint8_t adc_dma_ch = 1;
-static const uint32_t adc_sample_freq = 10000;
-static const uint8_t adc_buf_size = 3;
-static uint16_t adc_buf[adc_buf_size] = { 2047, 2047, 2047 };
+static const uint32_t adc_sample_freq = 8000;
+static const uint8_t adc_buf_size = 6;
+static uint16_t adc_buf[adc_buf_size];
 
 template<uint8_t CH> inline uint16_t read();
 
 template<> inline uint16_t read<0>() { return adc_buf[0]; }
-template<> inline uint16_t read<1>() { return adc_buf[1]; }
-template<> inline uint16_t read<2>() { return adc_buf[2]; }
+template<> inline uint16_t read<1>() { return adc_buf[2]; }
+template<> inline uint16_t read<2>() { return adc_buf[4]; }
 
 void setup()
 {
     adc_tim::setup(0, sys_clock::freq() / adc_sample_freq - 1);
     adc_tim::master_mode<adc_tim::mm_update>();
-    //adc_tim::update_interrupt_enable();
-    //hal::nvic<interrupt::TIM4>::enable();
+    adc_tim::update_interrupt_enable();
+    hal::nvic<interrupt::TIM4>::enable();
 
     adc_dma::setup();
-    adc::setup();
-    adc::sequence<1, 2, 15>();
+    adc::setup<6>();
+    adc::sample_time<2>();
+    adc::oversample<32>();
+    adc::sequence<1, 1, 2, 2, 15, 15>();
 
-    device::ADC1.CFGR2 |= device::adc1_t::CFGR2_ROVSE
-                       |  device::adc1_t::CFGR2_OVSR<0x3>
-                       |  device::adc1_t::CFGR2_OVSS<0x4>
-                       ;
-
-    //device::ADC1.IER |= device::adc1_t::IER_EOSIE; // enable end of sequence interrupt
-    //hal::nvic<interrupt::ADC1_2>::enable();
+    device::ADC1.IER |= device::adc1_t::IER_EOSIE; // enable end of sequence interrupt
+    hal::nvic<interrupt::ADC1_2>::enable();
 
     adc::dma<adc_dma, adc_dma_ch>(adc_buf, adc_buf_size);
     adc::trigger<0xc>();        // FIXME: use symbol for TIM4_TRGO
@@ -57,7 +54,13 @@ void setup()
 
 template<> void handler<interrupt::TIM4>()
 {
+    board::probe::clear();
     analog::adc_tim::clear_uif();
+}
+
+template<> void handler<interrupt::ADC1_2>()
+{
+    device::ADC1.ISR |= device::adc1_t::ISR_EOS; // clear end of sequence flag
     board::probe::set();
 }
 
