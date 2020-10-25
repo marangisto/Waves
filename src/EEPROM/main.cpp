274fae4d4f6d12d077d9f0d56aea55eb16f14f6e
Bengt Marten Agren <marangisto@me.com>
2020-10-25 14:59:48 +0800

    Properly working console.

--- src/EEPROM/main.cpp
+++ src/EEPROM/main.cpp
@@ -4,6 +4,7 @@
 #include <textio.h>
 #include <gpio.h>
 #include <spi.h>
+#include <timer.h>
 #include <st7789.h>
 #include <draw.h>
 #include <text.h>
@@ -13,13 +14,13 @@ using namespace fontlib;
 using namespace text;
 using namespace color;
 
-typedef st7789_t<1, PA5, PA7, PB1, PB4> display;
-static const spi_clock_divider_t display_spi_prescale = fpclk_8;
-
+using display = st7789_t<1, PA5, PA7, PB1, PB4>;
+using console = console_t<display>;
+using encoder = encoder_t<4, PB6, PB7>;
 using eeprom = at25_t<256, spi_t<2, PB13, PA11, PA10>, PB12>;
 
-template<typename TXR>
-static void hexdump(TXR& txr, const char *buf, uint16_t len, uint16_t base = 0)
+template<typename WRITER>
+static void hexdump(const char *buf, uint16_t len, uint16_t base = 0)
 {
     static constexpr unsigned bpl = 8; // bytes per line
     static char line[8 + 4 * bpl];
@@ -29,32 +30,52 @@ static void hexdump(TXR& txr, const char *buf, uint16_t len, uint16_t base = 0)
         char *lp = line;
 
         lp += sprintf(lp, "%04x ", base + i);
+
         for (unsigned j = 0; j < bpl; ++j)
             if (i + j < len)
                 lp += sprintf(lp, "%02x ", buf[i+j]);
             else
                 lp += sprintf(lp, "   ");
+
         for (unsigned j = 0; j < bpl; ++j)
             if (i + j < len)
                 lp += sprintf(lp, "%c", isprint(buf[i+j]) ? buf[i+j] : '.');
-        txr.writeln(line);
+
+        printf<WRITER>("%s\n", line);
     }
 }
 
 int main()
 {
-    display::setup<display_spi_prescale>();
+    display::setup<fpclk_8>();
+    console::setup(fontlib::cmuntt_16, color::green, color::black);
+    console::clear();
+    encoder::setup<pull_up>(65535);
     eeprom::setup();
 
-    font_t ft = fontlib::cmuntt_16;
-    text_renderer_t<display> txr(ft, color::green, color::black, true);
+    console::clear();
+    printf<console>("start!\n");
 
-    static char buf[128];
-    uint16_t addr = 0;
+    for (;;)
+    {
+        static uint16_t encoder_last_count = -1;
+        uint16_t c = ((-encoder::count()) >> 1) & 0x1ff;
 
-    if (eeprom::read(addr, buf, sizeof(buf)))
-        txr.writeln("read failure!");
-    else
-        hexdump(txr, buf, sizeof(buf), addr);
+        if (c != encoder_last_count)
+        {
+            static char buf[128];
+            uint32_t addr = sizeof(buf) * c;
+
+            console::clear();
+            printf<console>("page %3u 0x%04x\n\n", c, addr);
+
+            if (eeprom::read(addr, buf, sizeof(buf)))
+                printf<console>("read failure!\n");
+            else
+                hexdump<console>(buf, sizeof(buf), addr);
+        }
+
+        encoder_last_count = c;
+    }
 }
 
