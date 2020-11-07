295ed9f944b6a2807d59b3dd32f76bd7073a4ce8
Bengt Marten Agren <marangisto@me.com>
2020-11-07 09:39:47 +0800

    Rename kernels.

--- tools/Kernel/app/Main.hs
+++ tools/Kernel/app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Math.Combinatorics.Exact.Binomial
+--import Math.Combinatorics.Exact.Binomial
 import Control.Monad
 import Numeric (showHex)
 import qualified Data.Text as T
@@ -12,7 +12,7 @@ main = do
     putStr $ unlines
         [ "#include <fixed.h>"
         , ""
-        , "// Gaussian kernels normalized to 1 in Q31 fixed point format."
+        , "// Epanechnikov kernels normalized to 1 in Q31 fixed point format."
         , ""
         , "using namespace fixed;"
         ]
@@ -23,7 +23,7 @@ main = do
         putStrLn $ decl n $ map (q31 . (/s)) xs
     putStrLn $ unlines $
         [ ""
-        , "const q31_t *gauss_kernel(unsigned n)"
+        , "const q31_t *epanechnikov(unsigned n)"
         , "{"
         , "    switch (n)"
         , "    {"
@@ -35,7 +35,7 @@ main = do
         ]
     where f n = "    case "
              <> show n
-             <> ": return gauss_kernel_"
+             <> ": return epanechnikov"
              <> show n
              <> ";"
 
@@ -43,7 +43,7 @@ epanechnikov :: Int -> Int -> Double
 epanechnikov n i = 1 - x * x
     where x = 2 * (fromIntegral (i + 1) / fromIntegral (n + 2)) - 1
 
-decl n xs = "\nconstexpr q31_t gauss_kernel_" <> show n <> "[" <> show (n + 1) <> "] = \n"
+decl n xs = "\nconstexpr q31_t epanechnikov" <> show n <> "[" <> show (n + 1) <> "] = \n"
          <> "    { " <> ss <> "\n    };"
     where ss = intercalate "\n    , "
              $ map (intercalate ", " . words)
